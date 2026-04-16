5e402014817066860369f0294ec6fb89c2b16305
DoanDucThang0805 <ducthang8558@gmail.com>
2026-04-16 17:06:50 +0700

    new results

--- moe_inference.sh
+++ moe_inference.sh
@@ -11,7 +11,7 @@ echo "=========================================="
 
 python -m inference.moe.inference \
   --model-name mobilenetv3small_moe \
-  --run-time run_20260416-023523 \
+  --run-time run_20260416-152114 \
   --dataset-name plantdoc \
   --topk 2 \
   --num-expert 4 \
--- trainning.sh
+++ trainning.sh
@@ -38,6 +38,54 @@ python -m trainning.moe_train \
     --batch_size 32 \
     --lr 0.001 \
     --weight_decay 0.001 \
-    --moe_alpha 0.05 \
+    --moe_alpha 0.01 \
+    --temperature 0.5 \
+    --type_model moe_0.5temperature
+
+python -m trainning.moe_train \
+    --seed 42 \
+    --num_experts 4 \
+    --top_k 2 \
+    --num_epochs 300 \
+    --batch_size 32 \
+    --lr 0.001 \
+    --weight_decay 0.001 \
+    --moe_alpha 0.01 \
+    --temperature 0.5 \
+    --type_model moe_0.5temperature
+
+python -m trainning.moe_train \
+    --seed 42 \
+    --num_experts 4 \
+    --top_k 2 \
+    --num_epochs 300 \
+    --batch_size 32 \
+    --lr 0.001 \
+    --weight_decay 0.001 \
+    --moe_alpha 0.01 \
+    --temperature 0.5 \
+    --type_model moe_0.5temperature
+
+python -m trainning.moe_train \
+    --seed 42 \
+    --num_experts 4 \
+    --top_k 2 \
+    --num_epochs 300 \
+    --batch_size 32 \
+    --lr 0.001 \
+    --weight_decay 0.001 \
+    --moe_alpha 0.01 \
+    --temperature 0.5 \
+    --type_model moe_0.5temperature
+
+python -m trainning.moe_train \
+    --seed 42 \
+    --num_experts 4 \
+    --top_k 2 \
+    --num_epochs 300 \
+    --batch_size 32 \
+    --lr 0.001 \
+    --weight_decay 0.001 \
+    --moe_alpha 0.01 \
     --temperature 0.5 \
     --type_model moe_0.5temperature
