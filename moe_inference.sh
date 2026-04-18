fc133d0633dd86a73b86aeda9c9a810b95215dac
DoanDucThang0805 <ducthang8558@gmail.com>
2026-04-18 14:54:38 +0700

    ket qua moi

--- moe_inference.sh
+++ moe_inference.sh
@@ -11,12 +11,12 @@ echo "=========================================="
 
 python -m inference.moe.inference \
   --model-name mobilenetv3small_moe \
-  --run-time run_20260417-104039 \
+  --run-time run_20260418-120706 \
   --dataset-name plantdoc \
   --topk 2 \
   --num-expert 4 \
   --type-model moe_0.5temperature \
-  --seed 45
+  --seed 52
 
 echo "=========================================="
 echo "All inference completed!"
--- trainning.sh
+++ trainning.sh
@@ -7,31 +7,7 @@ cd src
 clear
 
 python -m trainning.moe_train \
-    --seed 45 \
-    --num_experts 4 \
-    --top_k 2 \
-    --num_epochs 300 \
-    --batch_size 32 \
-    --lr 0.001 \
-    --weight_decay 0.001 \
-    --moe_alpha 0.05 \
-    --temperature 0.5 \
-    --type_model moe_0.5temperature
-
-python -m trainning.moe_train \
-    --seed 45 \
-    --num_experts 4 \
-    --top_k 2 \
-    --num_epochs 300 \
-    --batch_size 32 \
-    --lr 0.001 \
-    --weight_decay 0.001 \
-    --moe_alpha 0.05 \
-    --temperature 0.5 \
-    --type_model moe_0.5temperature
-
-python -m trainning.moe_train \
-    --seed 45 \
+    --seed 50 \
     --num_experts 4 \
     --top_k 2 \
     --num_epochs 300 \
