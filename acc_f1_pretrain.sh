e68cdce2339705c05579f196a95821e86858f661
icnlab <ducthang8558@gmail.com>
2026-05-13 00:04:45 +0700

    switch code to context aware v2

--- acc_f1_pretrain.sh
+++ acc_f1_pretrain.sh
@@ -8,7 +8,7 @@ source venv1/bin/activate
 
 # Run the script
 python src/benchmark/getaccvsf1pretrain.py \
-    --checkpoint_dirs "/media/data/minhht/context_moe/checkpoints/plantdoc/pretrain_weight/custom_mobilenetv3_smallv1" \
+    --checkpoint_dirs "/media/icnlab/Data/Thang/plan_dieases/context_moe/checkpoints/plantdoc/pretrain_weight/efficientnetb0" \
     --csv_store_dir "./results" \
-    --csv_filename "custom_mobilenetv3_smallv1_results.csv" \
+    --csv_filename "efficientnetb0_results.csv" \
     --export_csv
