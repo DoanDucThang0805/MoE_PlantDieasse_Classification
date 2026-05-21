69c5ae56df4cdeb48bc41b33d24111284f025b7e
DoanDucThang0805 <ducthang8558@gmail.com>
2026-05-22 00:53:10 +0700

    Fix activation of virtual environment and update gating mechanism in MoE model for ONNX compatibility

--- export_onnx.sh
+++ export_onnx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source venv/bin/activate
+source venv1/bin/activate
 
 CHECKPOINT="/media/data/minhht/context_moe/checkpoints/plantdoc/moe_linearcontextaware_temp0.5/mobilenetv3small_moe/4_experts/top_2/seed_43/run_20260504-214159/best_checkpoint.pth"
 OUTPUT="/media/data/minhht/context_moe/onnx/mobilenetv3small_moe.onnx"
